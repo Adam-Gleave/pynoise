f97e7346aef37fe80c5af667c1e23418eaec79af
Adam Gleave <Adam-Gleave@users.noreply.github.com>
2017-11-18 10:53:19 +0100

    Add 2D functionality -- may be slightly buggy at certain values.

--- simplexnoise.py
+++ simplexnoise.py
@@ -4,10 +4,9 @@ import math
 # Simplex noise creation class
 class SimplexNoise:
     # Permutation table
-
     # Use this to hash a number to a random integer between 0 and 255.
     # Numbers in this array are a randomly organised set
-    __permutations = [
+    __PERMUTATIONS = [
         216, 117, 91, 54, 101, 12, 189, 9, 211, 14, 73, 94, 24, 89, 231, 63,
         58, 5, 41, 124, 68, 204, 82, 210, 195, 202, 142, 109, 72, 32, 165, 11,
         60, 214, 2, 74, 193, 118, 217, 213, 92, 105, 158, 227, 20, 138, 70,
@@ -30,11 +29,44 @@ class SimplexNoise:
     # Helper function
     # Hash a number using permutations table
     def __hash(self, n):
-        n = n % 255
+        n = n % 0xFF
 
-        return self.__permutations[n]
+        return self.__PERMUTATIONS[n]
 
-    # Helper function
+    # Skewing factors to translate co-ordinates into simplex space
+    __FACTOR2D_A = 0.5 * (math.sqrt(3.0) - 1.0)
+    __FACTOR2D_B = (3.0 - math.sqrt(3.0)) / 6.0
+
+    """
+    --------------------------- Vector functions ------------------------------
+    
+    Interpolating gradient values requires obtaining the dot (scalar) product.
+    This is usually a slow process, but since the gradient vectors we are
+    dealing only contains 0, 1, or -1, we can replace these functions with our
+    own.
+    
+    ---------------------------------------------------------------------------    
+    """
+
+    # Return the dot product of two 2D vectors
+    def __dot2d(self, vector, x, y):
+        return vector[0] * x + vector[1] * y
+
+    """ 
+    --------------------- Gradient functions and tables -----------------------
+    
+    In order to compute the noise value of a point, we must find the value of 
+    an interpolation function between pseudorandom gradient values of all 
+    neighbouring grid vertices. These are not fully random, or patterns would 
+    not be repeatable given identical input.
+    
+    These helper functions provide the means of determining these gradients 
+    given aspecific grid-point co-ordinate, and the gradient of the function at
+    a given distance from this grid-point (the noise lookup value).
+    
+    ---------------------------------------------------------------------------
+    """
+
     # Compute gradient vector from a 1D coordinate
     def __grad(self, n, x):
         # Obtain lower 4 bits of hash
@@ -49,14 +81,31 @@ class SimplexNoise:
         # Multiply gradient with distance from corner
         return grad * x
 
-    # Entrance function
+    # 3D gradient table
+    # Use this to obtain a gradient from a given index value generated in a
+    # pseudorandom manner from 3D co-ordinates
+    __GRADS3D = [
+        (1, 1, 0), (-1, 1, 0), (1, - 1, 0), (-1, -1, 0),
+        (1, 0, 1), (-1, 0, 1), (1, 0, -1), (-1, 0, -1),
+        (0, 1, 1), (0, - 1, 1), (0, 1, -1), (0, -1, -1)
+    ]
+
+    """
+    ----------------------------- Noise functions -----------------------------
+    
+    These public functions allow the user of the SimplexNoise class to look up
+    a value retrieved from the noise function at any given co-ordinate.
+    
+    ---------------------------------------------------------------------------
+    """
+
     # 1D simplex noise
-    def get_value(self, x):
-        # Get "corners" of coordinate (nearest integers)
+    def get_value_1D(self, x):
+        # Get "corners" of co-ordinate (nearest integers)
         c0 = math.floor(x)
         c1 = c0 + 1
 
-        # Get distances from coordinate to corners
+        # Get distances from co-ordinate to corners
         x0 = x - c0
         x1 = x0 - 1
 
@@ -72,3 +121,81 @@ class SimplexNoise:
         # Max value +/- 2.53125
         # Scale to within [-1, +1]
         return 0.395 * (cont0 + cont1)
+
+    # 2D simplex noise
+    def get_value_2D(self, x, y):
+        cont0, cont1, cont2 = 0.0, 0.0, 0.0
+
+        # Skew the input space to translate to simplex cell space
+        # Hairy factor for 2D
+        simplex = (x + y) * self.__FACTOR2D_A
+        i = math.floor(x + simplex)
+        j = math.floor(y + simplex)
+
+        k = (i + j) * self.__FACTOR2D_B
+
+        # Un-skew cell origin to standard space
+        origin_x = i - k
+        origin_y = j - k
+
+        # Get distances from origin to co-ordinate
+        x0 = x - origin_x
+        y0 = y - origin_y
+
+        # Determine which simplex the co-ordinate lies within
+        # In 2D space, the simplex is an equilateral triangle
+        # There are two (skewed) simplices in each 2D cell
+
+        # Obtain offsets for middle (second) corner in skewed (simplex) sapce
+        if x0 > y0:
+            i1 = 1
+            j1 = 0
+        else:
+            i1 = 0
+            j1 = 1
+
+        # Obtain offsets for middle (second) corner in standard space
+        x1 = x0 - i1 + self.__FACTOR2D_B
+        y1 = y0 - i1 + self.__FACTOR2D_B
+        # Obtain offsets for last corner in standard space
+        x2 = x0 - 1.0 + 2.0 * self.__FACTOR2D_B
+        y2 = y0 - 1.0 + 2.0 * self.__FACTOR2D_B
+
+        # Obtain an index into the gradient array at each simplex corner
+        # Wrap at 8 so as to not go out of bounds
+        ii = i & 255
+        jj = j & 255
+        grad0 = self.__hash(ii + self.__hash(jj)) % 12
+        grad1 = self.__hash(ii + i1 + self.__hash(jj + j1)) % 12
+        grad2 = self.__hash(ii + 1 + self.__hash(jj + 1)) % 12
+
+        # Calculate corner contributions
+        temp0 = 0.5 - x0 * x0 - y0 * y0
+
+        if temp0 < 0:
+            cont0 = 0
+        else:
+            temp0 = temp0 * temp0
+            gv = self.__GRADS3D[grad0]
+            cont0 = temp0 * temp0 * self.__dot2d(gv, x0, y0)
+
+        temp1 = 0.5 - x1 * x1 - y1 * y1
+
+        if temp1 < 0:
+            cont1 = 0.0
+        else:
+            temp1 = temp1 * temp1
+            gv = self.__GRADS3D[grad1]
+            cont1 = temp1 * temp1 * self.__dot2d(gv, x1, y1)
+
+        temp2 = 0.5 - x2 * x2 - y2 * y2
+
+        if temp2 < 0:
+            cont2 = 0.0
+        else:
+            temp2 = temp2 * temp2
+            gv = self.__GRADS3D[grad2]
+            cont2 = temp2 * temp2 * self.__dot2d(gv, x2, y2)
+
+        # Return the sum of all contributions, scaled to fit within [-1, 1]
+        return 70 * (cont0 + cont1 + cont2)
--- test/test.py
+++ test/test.py
@@ -5,18 +5,34 @@ import unittest
 class SimplexNoiseTest(unittest.TestCase):
     def setUp(self):
         self.noise = SimplexNoise()
+        test = self.noise.get_value_2D(1.3, 5.3)
+        print(test)
 
     def tearDown(self):
         self.noise = None
 
     # Test values against expected range
-    def test_correct_range(self):
-        for i in range(1, 10000):
+    def test_correct_range_1D(self):
+        for i in range(1, 1000):
             with self.subTest(i=i):
                 float_i = i / 100
-                result = self.noise.get_value(float_i)
+                result = self.noise.get_value_1D(float_i)
                 self.assertTrue(-1.0 <= result <= 1.0, "Noise value at" + str(float_i) +
                                 " is not between -1.0 and 1.0.")
+
+    # Test values against expected range (2D)
+    def test_correct_range_2D(self):
+        for x in range(1, 100):
+            for y in range(1, 100):
+                i = (x * 100) + y
+                with self.subTest(i=i):
+                    float_x = x / 100
+                    float_y = y / 100
+                    result = self.noise.get_value_2D(float_x, float_y)
+                    print(str(result))
+                    self.assertTrue(-1.0 <= result <= 1.0, "Noise value at" +
+                                    str(float_x) + "," + str(float_y) +
+                                    " is not between -1.0 and 1.0.")
 
 
 if __name__ == '__main__':
